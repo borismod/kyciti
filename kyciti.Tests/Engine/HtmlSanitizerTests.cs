fb9b2547e70ddc24d7aa639e2e3798282180197a
Boris Modylevsky <boris.modylevsky@citi.com>
2018-04-26 22:35:17 +0300

    add HtmlSanitizer

---
+++ kyciti.Tests/Engine/HtmlSanitizerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HtmlAgilityPack;
+using kyciti.Engine;
+using NUnit.Framework;
+
+namespace kyciti.Tests.Engine
+{
+    [TestFixture]
+    public class HtmlSanitizerTests
+    {
+        [Test]
+        public void SanitizeHtml_ExistingUrl_DoesNotContainHtmlTags()
+        {
+            // Arrange
+            var htmlWeb = new HtmlWeb();
+            var document = htmlWeb.Load(@"https://en.wikipedia.org/wiki/Strategy_pattern");
+
+            // Act
+            var htmlSanitizer = new HtmlSanitizer();
+            var cleanText = htmlSanitizer.SanitizeHtmlDocument(document);
+
+            // Assert
+            cleanText.Should()
+                .NotContain(@"<img")
+                .And.NotContain(@"<div")
+                .And.NotContain(@"<!DOCTYPE html>")
+                .And.NotContain(@"document.documentElement.className");
+        }
+    }
+}
---
+++ kyciti/Engine/HtmlSanitizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace kyciti.Engine
+{
+    public class HtmlSanitizer
+    {
+        private static readonly string[] BlackListNodeNames = {"script", "style", "#comment"};
+        private static readonly string[] WhiteListNodeNames = {"strong", "em", "u"};
+
+        public string SanitizeHtmlDocument(HtmlDocument document)
+        {
+            RemoveBlackListNodes(document);
+
+            LeaveWhiteListNodes(document);
+
+            return document.DocumentNode.InnerHtml;
+        }
+
+        private static void LeaveWhiteListNodes(HtmlDocument document)
+        {
+            var nodes = new Queue<HtmlNode>(document.DocumentNode.SelectNodes("./*|./text()"));
+            while (nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+                var parentNode = node.ParentNode;
+
+                if (!WhiteListNodeNames.Contains(node.Name) && node.Name != "#text")
+                {
+                    var childNodes = node.SelectNodes("./*|./text()");
+
+                    if (childNodes != null)
+                        foreach (var child in childNodes)
+                        {
+                            nodes.Enqueue(child);
+                            parentNode.InsertBefore(child, node);
+                        }
+
+                    parentNode.RemoveChild(node);
+                }
+            }
+        }
+
+        private static void RemoveBlackListNodes(HtmlDocument document)
+        {
+            document.DocumentNode.Descendants()
+                .Where(n => BlackListNodeNames.Contains(n.Name))
+                .ToList()
+                .ForEach(n => n.Remove());
+        }
+    }
+}
